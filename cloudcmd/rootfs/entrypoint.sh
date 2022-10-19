70b64459249f86229330c53c19f542c18bc73858
Barış Özdağ <barisozdag@gmail.com>
2022-10-19 23:51:04 +0300

    Update entrypoint.sh

--- cloudcmd/rootfs/entrypoint.sh
+++ cloudcmd/rootfs/entrypoint.sh
@@ -6,7 +6,4 @@ for SCRIPTS in /etc/cont-init.d/*; do
     echo "$SCRIPTS: executing"
     chown "$(id -u)":"$(id -g)" "$SCRIPTS"
     chmod a+x "$SCRIPTS"
-    # Change shebang if no s6 supervision
-    sed -i 's|/command/with-contenv bashio|/usr/bin/env bashio|g' "$SCRIPTS"
-    /."$SCRIPTS" || echo "$SCRIPTS: exiting $?"
 done
