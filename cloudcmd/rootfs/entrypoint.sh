10683d1c07846202bcd421d6c73e4d10c7d82ab5
Barış Özdağ <barisozdag@gmail.com>
2022-10-20 00:08:27 +0300

    remove entrypoint

--- cloudcmd/rootfs/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "Starting..."
-
-for SCRIPTS in /etc/cont-init.d/*; do
-    [ -e "$SCRIPTS" ] || continue
-    echo "$SCRIPTS: executing"
-    chown "$(id -u)":"$(id -g)" "$SCRIPTS"
-    chmod a+x "$SCRIPTS"
-done
